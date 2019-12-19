ca2e55ad7115233508abd5adf472211b15dc7eaa
jannikwiessler <56755425+jannikwiessler@users.noreply.github.com>
2019-12-19 15:36:31 +0100

    added Gui
    simple link between ui and engine via text file for specs
    UI as well as Engine are tested on mac os -> works well

---
+++ kMeansEngine.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 18:09:09 2019
+
+@author: jannik wiessler
+V1 dirty implementation to visualize Kmeans (unsupervised learning)
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# functions
+# ---------
+def rmvDataPoints(samples,centers):
+    if samples != -1:
+        samples.remove()
+    if centers != -1:
+        centers.remove()
+
+def calcDist(X,C):
+#   L_2-Norm
+    temp = X-C
+    temp = temp*temp
+    temp = temp.sum(axis=1)
+    temp = np.sqrt(temp) # to be L2 confirm, but not nedded
+    return temp
+    
+def calcCluster(X,C):
+    dist = np.zeros((X.shape[0],C.shape[0]))
+    for i in range(C.shape[0]): 
+        dist[:,i] = calcDist(X,C[i,:])
+    idx = np.argmin(dist,axis=1)
+    return idx
+    
+def colCluster(X,C,samplesFig,centersFig):
+    idx = calcCluster(X,C)
+    colSamples = []
+    for i in range(X.shape[0]):
+        colSamples.append(color[idx[i]]) 
+    rmvDataPoints(samplesFig,centersFig)    
+    samples = plt.scatter(X[:,0],X[:,1],c=colSamples,marker='.')
+    centers = plt.scatter(C[:,0],C[:,1],c=color[0:numOfCentInit],marker='s',edgecolors='black')  
+    plt.pause(pauseTimer)
+    return samples, centers, idx
+
+def calcCenter(X,idx):
+    center = np.zeros((numOfCentInit,2))
+    for i in range(max(idx)+1):
+        cluster = X[(idx == i),:]
+        center[i] = sum(cluster)/cluster.shape[0]
+    return center
+
+def avgDist(C,oldC):
+    dist = calcDist(C,oldC)
+    return sum(dist)/C.shape[0]
+    
+def moveCenter(X,C,idx,centersFig):
+    FLAGG = 0
+    oldC = C #remember old center-values
+    C = calcCenter(X,idx)
+    for i in range(C.shape[0]):
+        plt.plot((oldC[i][0],C[i][0]),(oldC[i][1],C[i][1]),
+                 color='grey', marker='', linestyle='dashed',linewidth=1, markersize=0)
+    rmvDataPoints(-1,centersFig)
+    centers = plt.scatter(C[:,0],C[:,1],c=color[0:numOfCentInit],marker='s',edgecolors='black')
+    plt.pause(pauseTimer) 
+    averageMove = avgDist(C,oldC)
+    if averageMove==0:
+        FLAGG = 1
+    return centers, C, FLAGG, averageMove
+    
+    
+# main 
+# ----
+pauseTimer = 0.05
+numOfSamples = 10
+numOfCentInit = 5
+loops = 20
+try:
+    with open('kMeanSpecs.txt', 'r') as filehandle:
+        specs = filehandle.readlines()
+    try:
+        numOfSamples = int(specs[0])
+        numOfCentInit = int(specs[1])
+        loops = int(specs[2])
+        pauseTimer = 0.5/float(specs[3])
+        # (assume zero execute time for code)
+        # 1 (update/sec) => pauseTimer = 0.5, as there are 2 plt.pause 
+        # ==> pauseTimer = 0.5/(#updates/sec)
+    except: pass
+except: pass
+
+print('numOfSamples: '+str(numOfSamples)) 
+print('numOfCentInit: '+str(numOfCentInit))
+print('numOfLoops: '+str(loops))
+print('pauseTimer:'+str(pauseTimer))
+
+color = ['blue','maroon','green','cyan','magenta','grey','purple',
+    'crimson','darkgreen','saddlebrown']
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+ax1.axis([-0.05, 1.05, -0.05, 1.05])    
+
+X = np.random.rand(numOfSamples,2)
+C = np.random.rand(numOfCentInit,2)
+
+Xout = np.zeros((X.shape[0],2+1))
+Cout = np.zeros((C.shape[0]*(loops+1),2))
+
+samplesFig = plt.scatter(X[:,0],X[:,1],c='black',marker='.')
+centersFig = plt.scatter(C[:,0],C[:,1],c=color[0:numOfCentInit],marker='s',edgecolors='black')
+plt.pause(1) 
+
+for i in range(loops):   
+    Cout[0+i*numOfCentInit:numOfCentInit+i*numOfCentInit,:] = C 
+    samplesFig,centersFig,idx = colCluster(X,C,samplesFig,centersFig)
+    centersFig,C,returnFLAGG, averageMove = moveCenter(X,C,idx,centersFig)
+    plt.title('Iteration: '+str(i)+' | avgCenterMove: '+str(round(averageMove,4)), 
+              fontdict=None, loc='center', pad=None)
+    plt.pause(0.000001)
+    if returnFLAGG:
+        break
+    
+Xout[:,0:2] = X
+Xout[:,2] = idx
+Cout[0+(i+1)*numOfCentInit:numOfCentInit+(i+1)*numOfCentInit,:] = C
+plt.show()
+
---
+++ kMeansUI.py
@@ -0,0 +1,74 @@
+"""
+Created on Mon Dec 19 13:09:12 2019
+
+@author: jannik wiessler
+UI for Kmeans
+"""
+import sys, os
+import wx
+import subprocess
+
+class Example(wx.Frame): 
+    def __init__(self, parent, title): 
+        super(Example, self).__init__(parent, title = title, size = (220, 200)) 
+             
+        self.InitUI()
+        self.Centre() 
+        self.Show()     
+   
+    def InitUI(self): 
+        panel = wx.Panel(self)
+		
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+		
+        fgs = wx.FlexGridSizer(5, 2, 10,10)
+            
+        UI_samples = wx.StaticText(panel, label = "samples") 
+        UI_centers = wx.StaticText(panel, label = "initail centers") 
+        UI_iters = wx.StaticText(panel, label = "max iterations")
+        UI_updates = wx.StaticText(panel, label = "updates/second")
+        UI_button1 = wx.Button(panel,label="Run")
+        UI_button1.Bind( wx.EVT_BUTTON, self.OnButton1)
+        UI_button2 = wx.Button(panel,label="Cancel")
+        UI_button2.Bind( wx.EVT_BUTTON, self.OnButton2)
+
+        self.tc1 = wx.SpinCtrl(panel,initial=100,max=9999,min=5) 
+        self.tc2 = wx.SpinCtrl(panel,initial=4,max=100,min=1) 
+        self.tc3 = wx.SpinCtrl(panel,initial=30,max=9999,min=1)
+        self.tc4 = wx.SpinCtrl(panel,initial=5,max=9999,min=1)
+
+        fgs.AddMany([(UI_samples), (self.tc1, 1, wx.EXPAND), (UI_centers),(self.tc2, 1, wx.EXPAND), 
+            (UI_iters, 1, wx.EXPAND), (self.tc3, 1, wx.EXPAND), (UI_updates, 1, wx.EXPAND), (self.tc4, 1, wx.EXPAND),
+            (UI_button2, 1, wx.EXPAND), (UI_button1, 1, wx.EXPAND)])  
+        #fgs.AddGrowableRow(2, 1) 
+        #fgs.AddGrowableCol(1, 1)  
+        hbox.Add(fgs, proportion = 2, flag = wx.ALL|wx.EXPAND, border = 15) 
+        panel.SetSizer(hbox) 
+		
+    def OnButton1(self,push):
+        # get current directory: exe must be in same folder !
+        if getattr(sys, 'frozen', False): 
+            application_path = os.path.dirname(sys.executable)
+        elif __file__:
+            application_path = os.path.abspath(__file__)
+            application_path = application_path[0:-(len(os.path.basename(__file__))+1)]
+        callStr = 'python '+application_path+'/kMeansEngine.py'
+        self.writeSpecs()
+        os.popen(callStr)
+        
+    def OnButton2(self,push):
+        print('cancel')  
+        self.Close()   
+
+    def writeSpecs(self):
+        with open('kMeanSpecs.txt', 'w') as filehandle:
+            filehandle.write(
+            str(self.tc1.GetValue())+'\n'+
+            str(self.tc2.GetValue())+'\n'+
+            str(self.tc3.GetValue())+'\n'+
+            str(self.tc4.GetValue())
+            )    
+
+app = wx.App() 
+Example(None, title = 'kMeans specs') 
+app.MainLoop()
